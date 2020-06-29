0ab230b15a007ce92237ed83af4dda9d8a9a6ffc
Lucas Dias Lopes <lucas.fulldev@gmail.com>
2020-06-28 22:02:33 -0300

    configurando o app para continuar no ga

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const puppeteer = require('puppeteer');
 
+const NUMERO_VEZES = 10; // Numero de vezes que o app vai rodar!!!
 const url = 'http://www.thwpads.com';
-async function accessPage(index) {
+const timer = (Math.floor(Math.random() * (10 - 3) + 3) * 1000);
+
+async function accessPage() {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
+    
+    // await page.setRequestInterception(true);
+    await page.setUserAgent('Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Mobile Safari/537.36');
+    
     await page.goto(url);
-    await page.waitFor(10000);
+    await page.waitFor(timer);
 
     await browser.close();
 }
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < NUMERO_VEZES; i++) {
     setTimeout(() => {
-        (async () => accessPage(i))();
-    }, (10000 * i));
+        (async () => accessPage())();
+    }, (timer * i));
 }
